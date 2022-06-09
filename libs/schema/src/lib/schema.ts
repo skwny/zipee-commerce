c6c871ba2cf93a8eee4df6617f536c6384f0398d
skwny <schoonoverdane@gmail.com>
2022-06-08 22:30:23 -0700

    first draft of schema

--- libs/schema/src/lib/schema.ts
+++ libs/schema/src/lib/schema.ts
@@ -1,3 +1,25 @@
 export function schema(): string {
   return 'schema';
 }
+
+export interface I_Store {
+  count_discounts_given: number
+  count_discounts_used: number
+  count_store_purchases: number
+  current_discount_id: string
+  nth: number
+}
+
+export interface I_Customer {
+  id: string
+  count_purchase: number
+  // This is the foreign key for I_Discount collection id
+  discount?: string
+}
+
+export interface I_Discount {
+  id: string
+  code: string
+  // This is arbitrary due to project scope and can be a percent, dollar amount, etc.
+  amount: number
+}
